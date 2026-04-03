766a7bb12dab5d3e4b9232d391cfd37e82ff6a53
Dakshit Jha <153517628+Dakshitj14@users.noreply.github.com>
2026-04-03 15:43:39 +0530

    Remove divide tests and add print statement

--- test_math.py
+++ test_math.py
@@ -1,9 +1,2 @@
-from math_utils import divide
-
-
-def test_divide():
-    assert divide(10, 0) == 0
-    assert divide(10, 2) == 5
-    assert divide(9, 3) == 3
-    assert divide("10", 2) == 5
-    assert divide(8, 2) == 4
+print(x)
+x = 10
